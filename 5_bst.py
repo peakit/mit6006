42754bd08c4351d8af70d4696f2c0edebec8bb8d
peakit <hmanak@gmail.com>
2022-06-18 22:08:19 +0530

    BST Tree
    Handled next-larger/successor and delete operations

--- 5_bst.py
+++ 5_bst.py
@@ -17,16 +17,16 @@ class Node:
         self.parent = parent
         self.left = left
         self.right = right
-    
+
     def __str__(self):
-        s = "key: " + str(self.key)
+        s = "key= " + str(self.key)
         if self.parent is not None:
-            s = s + ", parent: " + str(self.parent.key)
+            s = s + ", parent=" + str(self.parent.key)
         if self.left is not None:
-            s = s + ", left: " + str(self.left.key)
+            s = s + ", left=" + str(self.left.key)
         if self.right is not None:
-            s = s + ", right :" + str(self.right.key)
-        
+            s = s + ", right=" + str(self.right.key)
+
         return s
 
 
@@ -34,7 +34,7 @@ class Tree:
 
     def __init__(self, root):
         self.root = root
-    
+
     @staticmethod
     def _rec_inorder(node):
         if node is None:
@@ -56,8 +56,77 @@ class Tree:
                 current = current.right
             else:
                 current = current.left
-        
         return None
+
+    def successor(self, val):
+        """
+        Successor is also known as "next larger"
+
+        Two cases here:
+        1. When the node has right subtree then successor is 
+            min value from this right subtree
+        2. When the node has no right subtree then the successor
+            is that parent of this node which has any left child
+        """
+        found = self.search(val)
+        if found is None:
+            return None
+
+        if found.right is not None:
+            # Right subtree exists,
+            # find min from this sub tree
+            rnode = found.right
+            while rnode.left is not None:
+                rnode = rnode.left
+            return rnode
+        else:
+            # Right subtree does not exist,
+            # keep following parent pointers
+            # till you find a node which is left
+            # child of its parent. That's the
+            # successor
+            if found.parent is None:
+                # basically asking for successor of a root node
+                # and when the root has no right subtree
+                return None
+            
+            while found.parent is not None and \
+                    found != found.parent.left:
+                found = found.parent
+            return found
+
+    def delete(self, val):
+        node = self.search(val)
+        if node is None:
+            raise Exception("Node to be deleted is not found!")
+
+        # if it is leaf node then just kill it!
+        if node.left is None and node.right is None:
+            if node.parent is not None and node.parent.left == node:
+                # if the node was left child of its parent
+                node.parent.left = None
+            elif node.parent is not None and node.parent.right == node:
+                # node is right child of its parent
+                node.parent.right = None
+
+            # if we just deleted the root node then set the self.root ptr accordingly
+            if node == self.root:
+                self.root = None
+            del node
+
+        else:
+            # intermediate node, just replace with successor's key
+            succ = self.successor(val)
+            node.key = succ.key
+            if succ.parent.left == succ:
+                succ.parent.left = succ.right
+                succ.right.parent = succ.parent
+                del succ
+            elif succ.parent.right == succ:
+                succ.parent.right = succ.right
+                succ.right.parent = succ.parent
+                del succ
+
 
 def build_bst(arr):
     if len(arr) < 1:
@@ -101,7 +170,7 @@ def capture_inputs():
 def main():
     arr = capture_inputs()
     tree = build_bst(arr)
-    
+
     print("Inorder traversal ")
     tree.inorder_traversal()
 
@@ -111,7 +180,15 @@ def main():
     if found is None:
         print("Value not found!")
     else:
-        print("Value found: ", found)   # Node's default __str__ method will fire
+        # Node's default __str__ method will fire
+        print("Value found: ", found)
+
+    succ = tree.successor(val)
+    print("Successor: ", succ)
+
+    tree.delete(val)
+    print("\nInorder traversal after deleting node with value ", val)
+    tree.inorder_traversal()
 
 
 if __name__ == "__main__":
