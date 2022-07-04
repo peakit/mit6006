a10554800f617b8927b71cdb7f21afb3c3140501
peakit <hmanak@gmail.com>
2022-07-04 18:12:39 +0530

    Karp Rabin rolling hash algorithm to find substrings
    Average: O(t+s)
    Worst: O(t*s) => when each sliding window results in
    equal hash value and hence requires each characted
    of the string to be compared/equated

---
+++ 9_karp_rabin_rolling_hash.py
@@ -0,0 +1,39 @@
+"""
+Karp Rabin's rolling hash algorithm to do string matching
+
+Relies on:
+- using a sliding window
+- calculating hash of each |s| width substring
+- if the hash match then only match the string
+"""
+
+
+def search(t, s):
+    indices = []
+    for i in range(len(t) - len(s)):
+        astring = t[i:i+len(s)]
+        if hash(s) == hash(astring) and \
+                s == astring:
+            indices.append(i)
+    return indices
+
+
+def capture_inputs():
+    print("Enter a long string=", end="")
+    raw = input()
+    t = raw
+    print("Enter the substring to be searched=", end="")
+    raw = input()
+    s = raw
+
+    return t, s
+
+
+def main():
+    t, s = capture_inputs()
+    indices = search(t, s)
+    print("=>The substring is present at following indices=", indices)
+
+
+if __name__ == "__main__":
+    main()
